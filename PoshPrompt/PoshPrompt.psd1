bbbb73e1c141b7a5b92222ef42f2f3804bd868b3
Zeke Abshire <zekeabshire@gmail.com>
2025-12-16 22:55:32 -0600

    Created a loading spinner

--- PoshPrompt/PoshPrompt.psd1
+++ PoshPrompt/PoshPrompt.psd1
@@ -68,6 +68,7 @@
         'Invoke-Text'
         'Invoke-Confirm'
         'Invoke-Select'
+        'Invoke-Spinner'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ PoshPrompt/functions/Invoke-Spinner.ps1
@@ -0,0 +1,27 @@
+﻿function Invoke-Spinner {
+    [CmdletBinding()]
+    param([Parameter(Mandatory)][string]$Message, [Parameter(Mandatory)][scriptblock]$ScriptBlock)
+
+    $frames = @('⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏')
+    $job = Start-Job -ScriptBlock $ScriptBlock
+
+    [Console]::CursorVisible = $false
+    try {
+        $i = 0
+        while ($job.State -eq 'Running') {
+            $frame = $frames[$i % $frames.Count]
+            [Console]::SetCursorPosition(0, [Console]::CursorTop)
+            Write-Host "$(Format-AnsiText $frame -Foreground 'Magenta')  $Message" -NoNewline
+            Start-Sleep -Milliseconds 80
+            $i++
+        }
+        $result = Receive-Job -Job $job
+        [Console]::SetCursorPosition(0, [Console]::CursorTop)
+        Write-Host "$(Format-AnsiText '◇' -Foreground 'Green') $Message"
+        return $result
+    }
+    finally {
+        [Console]::CursorVisible = $true
+        if ($job) { Remove-Job -Job $job -Force -ErrorAction SilentlyContinue }
+    }
+}
