a5187e40926ca3ce21a6b4f55de6f7fac0d526d7
Zeke Abshire <zekeabshire@gmail.com>
2025-12-16 22:31:44 -0600

    Created simple Intro/Outro macro prompts

---
+++ PoshPrompt/functions/Write-Intro.ps1
@@ -0,0 +1,9 @@
+﻿function Write-Intro {
+    [CmdletBinding()]
+    param([Parameter(Mandatory)][string]$Title)
+    
+    $symbolStart = Format-AnsiText "┌" -Style 'Dim'
+    $intro = Format-AnsiText " $Title " -Foreground 'Cyan' -Style 'Inverse'
+    Write-Host "`n$symbolStart  $intro"
+    Write-Host (Format-AnsiText "│" -Style 'Dim')
+}
---
+++ PoshPrompt/functions/Write-Outro.ps1
@@ -0,0 +1,8 @@
+﻿function Write-Outro {
+    [CmdletBinding()]
+    param([Parameter(Mandatory)][string]$Message)
+    
+    Write-Host (Format-AnsiText "│" -Style 'Dim')
+    $symbolEnd = Format-AnsiText "└" -Style 'Dim'
+    Write-Host "$symbolEnd  $Message`n"
+}
