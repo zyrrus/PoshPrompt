29d2810722891d54ae3d70ee9d33a4af8ede5c4d
Zeke Abshire <zekeabshire@gmail.com>
2025-12-17 22:34:13 -0600

    Created a multiselect prompt

---
+++ PoshPrompt/functions/Invoke-MultiSelect.ps1
@@ -0,0 +1,19 @@
+﻿function Invoke-MultiSelect {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string]$Message,
+        [Parameter(Mandatory)][string[]]$Options,
+        [scriptblock]$Validate = $null
+    )
+
+    $initialState = New-PromptState
+    $initialState.HoveredIndex = 0
+    $initialState.SelectedIndices = [System.Collections.Generic.HashSet[int]]::new()
+
+    $renderFn = { param($s) Render-MultiSelectPrompt -State $s -Message $Message -Options $Options }
+    $updateFn = { param($s, $k) Update-MultiSelectStateFromKey -State $s -Key $k -Options $Options -Validate $Validate }
+
+    $finalState = Invoke-RenderLoop -InitialState $initialState -RenderFunction $renderFn -UpdateFunction $updateFn
+    if ($finalState.Status -eq [PromptStatus]::Cancelled) { throw "Operation cancelled by user" }
+    return $finalState.Value
+}
---
+++ PoshPrompt/internal/functions/prompts/multiselect/Update-MultiSelectStateFromKey.ps1
@@ -0,0 +1,45 @@
+﻿function Update-MultiSelectStateFromKey {
+    param(
+        [hashtable]$State,
+        [System.ConsoleKeyInfo]$Key,
+        [string[]]$Options,
+        [scriptblock]$Validate = $null
+    )
+
+    $newState = $State.Clone()
+    if (-not $newState.ContainsKey('HoveredIndex')) { $newState.HoveredIndex = 0 }
+    if (-not $newState.ContainsKey('SelectedIndices')) { $newState.SelectedIndices = [System.Collections.Generic.HashSet[int]]::new() }
+
+    switch ($Key.Key) {
+        'UpArrow' { $newState.HoveredIndex = if ($newState.HoveredIndex -gt 0) { $newState.HoveredIndex - 1 } else { $Options.Count - 1 } }
+        'DownArrow' { $newState.HoveredIndex = if ($newState.HoveredIndex -lt ($Options.Count - 1)) { $newState.HoveredIndex + 1 } else { 0 } }
+        'Spacebar' { 
+            if ($newState.SelectedIndices.Contains($newState.HoveredIndex)) { 
+                [void]$newState.SelectedIndices.Remove($newState.HoveredIndex) 
+            }
+            else { 
+                [void]$newState.SelectedIndices.Add($newState.HoveredIndex) 
+            }
+        }
+        'Enter' { 
+            $newState.Value = $Options[$newState.SelectedIndices] 
+            if ($Validate) {
+                $err = & $Validate $newState.Value
+                if ($err) {
+                    $newState.Status = [PromptStatus]::Error
+                    $newState.Feedback = $err 
+                }
+                else { $newState.Status = [PromptStatus]::Submitted }
+            }
+            else { $newState.Status = [PromptStatus]::Submitted }
+        }
+        'Escape' { $newState.Status = [PromptStatus]::Cancelled }
+        'C' { 
+            if ($Key.Modifiers -band [ConsoleModifiers]::Control) {
+                $newState.Status = [PromptStatus]::Cancelled
+            }
+        }
+    }
+
+    return $newState
+}
